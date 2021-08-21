ef3b7309956d32b965012517083ca263b5f8262d
pearcej <jan_pearce@berea.edu>
2021-08-21 14:14:25 -0400

    add comments to help students

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 /*  Have yov ever heard of rubber duck debugging?
     See https://en.wikipedia.org/wiki/Rubber_duck_debugging 
-    Çode and ASCII art by Dr. Jan Pearce, Berea College
+    Original code and ASCII art by Dr. Jan Pearce, Berea College
+
+    Edits made by:
+    TODO: <Add name of student 1 Here> -- to be added by student 1
+    TODO: <Add name of student 2 Here> -- to be added by student 2
+    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line
  */
 #include <iostream>
 using namespace std;
@@ -14,11 +19,11 @@ int main() {
     cout << " How many rubber ducks do you want to see? ";
     cin >> ducknum;
 
-    for (int i = 0; i < ducknum; i++) {
-        cout << "                __     " << endl;
-        cout << "              <(o )___-" << endl;
-        cout << "               ( .__> /" << endl;
-        cout << "                `----' " << endl;
+    for (int i = 0; i < ducknum; i++) { 
+        cout << "                __     " << endl; // TODO: student 1 may change this line and may add additional lines of art above
+        cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and the blotto of the duck
+        cout << "               ( .__> /" << endl; // student 3(2) may change this line
+        cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
     }
 
     return 0;
